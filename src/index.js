e70093564ec5c46a70e195e1b166c7cc0fc11ca4
Zahira <zahira.rosales@gmail.com>
2019-09-29 17:03:27 -0500

    Reto 05, 4 problemas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://rickandmortyapi.com/api/character/';
+//const API = 'https://us-central1-escuelajs-api.cloudfunctions.net/characters';
+localStorage.clear();
+
 
 const getData = api => {
   fetch(api)
@@ -19,12 +22,29 @@ const getData = api => {
       newItem.classList.add('Items');
       newItem.innerHTML = output;
       $app.appendChild(newItem);
+      localStorage.setItem('next_fetch', response.info.next);
     })
     .catch(error => console.log(error));
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = async function() {
+  try{
+    const next_fetch = localStorage.getItem('next_fetch');
+    if (next_fetch){
+      await getData(next_fetch);
+    }
+    else if (typeof next_fetch === 'string' && next_fetch==""){
+      console.log("Ya no hay mas personajes");
+      alert ("Ya no hay mas personajes");
+      intersectionObserver.unobserve($observe);
+    }
+    else {
+      await getData(API);
+    } 
+  }
+  catch(error) {
+    console.log(error)
+  }
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
